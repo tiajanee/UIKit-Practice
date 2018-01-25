8f5e53b3622120250a016543dae14577ca37a91f
Tia King <tiajaneeking@gmail.com>
2018-01-24 17:29:24 -0800

    Successfully made customCellViews, constraints still off

---
+++ TableViewPractice/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  TableViewPractice
+//
+//  Created by Tia King on 1/24/18.
+//  Copyright © 2018 Tia King. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TableViewPractice/customViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  customViewCell.swift
+//  TableViewPractice
+//
+//  Created by Tia King on 1/24/18.
+//  Copyright © 2018 Tia King. All rights reserved.
+//
+
+import UIKit
+
+class customViewCell: NSObject {
+
+}
