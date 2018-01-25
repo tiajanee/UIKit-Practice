e314589bb644c1fa675260df817db1512d020839
Tia King <tiajaneeking@gmail.com>
2018-01-24 17:30:32 -0800

    Adding UIimages

--- TableViewPractice/CustomTableViewCell.swift
+++ TableViewPractice/CustomTableViewCell.swift
@@ -10,15 +10,14 @@ import UIKit
 
 class CustomTableViewCell: UITableViewCell {
 
+   
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var landImage: UIImageView!
+    @IBOutlet weak var landLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
 }
--- TableViewPractice/ViewController.swift
+++ TableViewPractice/ViewController.swift
@@ -8,16 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let photos = ["bench", "flowers", "forest", "river", "lake", "bench", "flowers", "forest", "river", "lake", "bench", "flowers", "forest", "river", "lake"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photos.count
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! CustomTableViewCell
+        
+//        cell.cellView.layer.cornerRadius = cell.cellView.frame.height
+//        cell.landImage.layer.cornerRadius = cell.landImage.frame.height
+        cell.landLabel.text = photos[indexPath.row]
+        cell.landImage.image = UIImage(named: photos[indexPath.row])
+        return cell
     }
 
 
